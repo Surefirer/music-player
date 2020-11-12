1d448ef423ab47903c9be91eb38e4151804bbcd6
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-11-12 11:17:48 -0800

    fix some bugs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Song from "./components/Song";
 import Library from "./components/Library";
 import Nav from "./components/Nav";
 //import util
-import data from "./util";
+import data from "./data";
 
 function App() {
   const audioRef = useRef(null);
